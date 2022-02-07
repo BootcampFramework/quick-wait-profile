693c63f15895891fdfa4afd5351df9eb98649656
josecontifrwk <joseconti@frwk.com.br>
2022-02-07 11:42:01 -0300

    tela-login
    tela de login do paciente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { ParcelModule } from 'single-spa-angular/parcel';
-
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [AppComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule, ParcelModule],
+  declarations: [AppComponent, TestComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, ParcelModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
