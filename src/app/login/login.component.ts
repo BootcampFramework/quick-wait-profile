dc1083ab6e8b55fb0269c7feb80b9078d2dd5d00
AdrianoValadaresFrwk <95626253+AdrianoValadaresFrwk@users.noreply.github.com>
2022-02-10 10:11:17 -0300

    Ajustes nas rotas conforme a US (#6)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,39 +4,35 @@ import { navigateToUrl } from 'single-spa';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
-
 export class LoginComponent implements OnInit {
   formulario: FormGroup;
-  hasError: boolean
+  hasError: boolean;
   constructor(private formBuilder: FormBuilder) {
-    this.hasError = false
+    this.hasError = false;
     this.formulario = this.formBuilder.group({
       usuario: [null, Validators.required],
-      senha: [null, Validators.required]
-    })
+      senha: [null, Validators.required],
+    });
   }
 
-  ngOnInit(): void {
-
-  }
-
+  ngOnInit(): void {}
 
   //validação de erros
 
   verificaValidTouched(campo: any) {
-    return !this.formulario.get(campo)?.valid && this.formulario.get(campo)?.touched
+    return (
+      !this.formulario.get(campo)?.valid && this.formulario.get(campo)?.touched
+    );
   }
   onSubmit() {
-    const { usuario, senha } = this.formulario.value
-    if (usuario === "framework" && senha === "frame") {
-      localStorage.setItem('auth', JSON.stringify(this.formulario.value))
-      location.assign('/patient');
+    const { usuario, senha } = this.formulario.value;
+    if (usuario === 'framework' && senha === 'frame') {
+      localStorage.setItem('auth', JSON.stringify(this.formulario.value));
+      location.assign('/');
     } else {
-      this.hasError = true
+      this.hasError = true;
     }
-
-
   }
 }
